149706168d02fcadd7e3759701ab4f5746b9af2a
Petr Strbacka <petr.strbacka@tietoevry.com>
2025-10-17 16:44:44 +0200

    Added yeti theme; Changed Services section; Added Team section

--- js/startbootstrap.js
+++ js/startbootstrap.js
@@ -3,9 +3,6 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
 */
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
